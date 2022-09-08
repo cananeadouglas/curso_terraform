1f6e580ea1c224a22550ac5aca2718faec1ee60b
Douglas Cananéa <cananeadouglas@gmail.com>
2022-09-08 18:20:13 -0300

    add aula05 elastic ip, output e proprietario do ubuntu

---
+++ aula05caiodelgado/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = "0.14.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_shared_credentials_file
+  profile                 = var.aws_profile
+
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  tags          = var.instance_tags
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes = [
+
+    ]
+  }
+
+}
+
+# criando Elastic IP
+resource "aws_eip" "elasticip" {
+  instance = aws_instance.web.id
+}
